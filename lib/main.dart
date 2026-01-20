05e19b9edcf07ace01f65bc065f112deac7900b6
Mahmoudelnagar5 <elnagarm852@gmail.com>
2026-01-20 23:57:34 +0200

    Integrate Firebase and Google Maps functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'widgets/custom_google_map.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -9,12 +14,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: Scaffold(body: CustomGoogleMap()));
   }
 }
---
+++ lib/widgets/custom_google_map.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomGoogleMap extends StatefulWidget {
+  const CustomGoogleMap({super.key});
+
+  @override
+  State<CustomGoogleMap> createState() => _CustomGoogleMapState();
+}
+
+class _CustomGoogleMapState extends State<CustomGoogleMap> {
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(31, 29),
+      zoom: 10,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(initialCameraPosition: initialCameraPosition);
+  }
+}
+
+// zoom levels:
+// world view 0 -> 3
+// country view 4 -> 6
+// city view 10 -> 12
+// street view 13 -> 17
+// building view 18 -> 20
