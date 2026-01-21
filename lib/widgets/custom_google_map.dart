04a148d6632e2cff23aad7718df3bc11b6201d48
Mahmoudelnagar5 <elnagarm852@gmail.com>
2026-01-21 15:20:11 +0200

    Enhance CustomGoogleMap with location confirmation button and camera bounds

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -10,19 +10,52 @@ class CustomGoogleMap extends StatefulWidget {
 
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
+  late GoogleMapController googleMapController;
 
   @override
   void initState() {
     super.initState();
     initialCameraPosition = const CameraPosition(
-      target: LatLng(31, 29),
+      target: LatLng(30, 29),
       zoom: 10,
     );
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GoogleMap(initialCameraPosition: initialCameraPosition);
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: initialCameraPosition,
+          onMapCreated: (controller) => googleMapController = controller,
+          cameraTargetBounds: CameraTargetBounds(
+            LatLngBounds(
+              southwest: LatLng(30.0, 28.0),
+              northeast: LatLng(32.0, 30.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 16,
+          right: 16,
+          left: 16,
+          child: ElevatedButton(
+            onPressed: () {
+              googleMapController.animateCamera(
+                CameraUpdate.newLatLng(LatLng(31.0, 29.0)),
+              );
+            },
+            child: const Text('Confirm Location'),
+          ),
+        ),
+      ],
+    );
   }
 }
 
